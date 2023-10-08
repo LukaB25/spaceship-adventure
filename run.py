29bcbd93e8f58b7899ad5407196a7881eb45ad43
LukaB25 <>
2023-10-08 12:28:59 +0000

    Creating a starting story for the game.

--- run.py
+++ run.py
@@ -1,1 +1,20 @@
 print("SPACESHIP ADVENTURE \n")
+
+print("Welcome to your spaceship adventure!")
+print("It is a year 3076. You are an interplanetary traveler, \
+at least that is what they said you will be. \n")
+print("As due to an overpopulation on our home planet Earth, \n\
+the collective government devised a plan to send three million \n\
+people to the newly discovered habitable planet.")
+print("You have left your planet for a long trip \
+through the vast space in search of a better life.\n")
+print("You are suddenly awoken. You woke up before all other travelers, \
+and you need to find out what is going on.")
+print("Are you ready to open and leave your hibernation pod?\n")
+
+
+def open_hibernation_pod():
+    print("Hibernation pod opened")
+
+
+open_hibernation_pod()
