c3b2ff2e1427bdb46aa12ec18841a1a419cb415a
LukaB25 <>
2023-10-08 19:17:42 +0000

    add functions to keep track of wound state

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 print("SPACESHIP ADVENTURE \n")
 
 def start_game_message():
+    """
+    Prints out the welcome message and some details to set the scene.
+    Offers some insight in the game for the user.
+    """
     print("Welcome to your spaceship adventure!")
     print("It is a year 3076. You are an interplanetary traveler, "
           "at least that is what they said you will be. \n")
@@ -11,7 +15,7 @@ def start_game_message():
           "through the vast space in search of a better life.\n")
     print("You are suddenly awoken. You woke up before all other travellers, "
           "and you need to find out what is going on.")
-    print("Are you ready to open and leave your hibernation pod? [y/n]\n")
+    print("Are you ready to open and leave your hibernation pod? [y/n]")
 
 
 def open_hibernation_pod():
@@ -20,8 +24,6 @@ def open_hibernation_pod():
     Runs a while loop until user decides to leave the hibernation pod.
     Sets up the decision on how to progress through the game.
     """
-    start_game_message()
-
     while True:
         pod_choice = input("> ").lower()
         if pod_choice == "yes" or pod_choice == "y":
@@ -47,22 +49,36 @@ def restart_game_choice():
     restart_choice = input("Do you want to restart the game? [y/n] \n> ")
     if restart_choice == "y" or restart_choice == "yes":
         print("\nRestarting game!\n")
+        start_game_message()
         open_hibernation_pod()
     elif restart_choice == "n" or restart_choice == "no":
         print("Thank you for playing!")
         print("Closing the game")
+        quit()
 
 
 fall_choice = None
+cut_state = 0
 
-fall_choice = open_hibernation_pod()
 
-
-def bleeding_wound():
+def bleeding_wound(cut_state):
     """
     Handles user choice to fall and deals damage until user either finds
-    bandages or ends up bleeding to death.
+    bandages or reaches critical state.
     """
+    
+    cut_state += 1
+    if cut_state >=5:
+        print("You are slowly bleeding out, "
+              "find some bandages quickly.")
+    elif cut_state == 6:
+        print("You reached critical state.")
+        print("You died.")
+        quit()
+    return cut_state
+
+
+cut_state = bleeding_wound(cut_state)
 
 
 def choose_path(fall_choice):
@@ -92,25 +108,33 @@ take? [a/b] \n> ").lower()
             # future function that will handle bleeding outcome
         else:
             print("Invalid input. Please choose [a/b]")
+            quit()
 
+def main():
+    start_game_message()
+    fall_choice = open_hibernation_pod()
 
-if fall_choice == "a":
-    print("You managed to grab onto the pod door")
-    print("You regained your balance and start exploring the room.")
-    print("You see two paths, you can either go straight \
-towards the ship deck, or to the right towards the infirmary.")
-    choose_path("a")
-elif fall_choice == "b":
-    print("You accidentally grabbed the electrical cord and got electrocuted.")
-    print("You died.")
-    print("The End.")
-    restart_game_choice()
-elif fall_choice == "c":
-    print("You fell and cut your hand; you are bleeding.")
-    print("You should find some bandages to cover the wound.")
-    print("You start exploring the room.")
-    print("You see two paths, you can either go straight towards the deck[a] \
-or to the right towards the infirmary.[b]")
-    choose_path("c")
-else:
-    print("Invalid input, please choose [a/b/c]")
+    if fall_choice == "a":
+        print("You managed to grab onto the pod door")
+        print("You regained your balance and start exploring the room.")
+        print("You see two paths, you can either go straight "
+              "towards the ship deck, or to the right towards the infirmary.")
+        choose_path("a")
+    elif fall_choice == "b":
+        print("You accidentally grabbed the electrical cord and got electrocuted.")
+        print("You died.")
+        print("The End.")
+        restart_game_choice()
+    elif fall_choice == "c":
+        print("You fell and cut your hand; you are bleeding.")
+        print("You should find some bandages to cover the wound.")
+        print("You start exploring the room.")
+        print("You see two paths, you can either go straight towards the deck[a] "
+              "or to the right towards the infirmary.[b]")
+        choose_path("c")
+    else:
+        print("You fell and back into the hibernation pod and lost consciousness.")
+        restart_game_choice()
+
+if __name__ == "__main__":
+    main()
