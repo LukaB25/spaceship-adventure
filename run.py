0308ab7b0a9dce6943074ac04605e52d6ca0388f
LukaB25 <>
2023-10-08 14:58:17 +0000

    create some structure and add more functions to the game

--- run.py
+++ run.py
@@ -1,16 +1,17 @@
 print("SPACESHIP ADVENTURE \n")
 
-print("Welcome to your spaceship adventure!")
-print("It is a year 3076. You are an interplanetary traveler, \
-at least that is what they said you will be. \n")
-print("As due to an overpopulation on our home planet Earth, \
-the collective government devised a plan to send three million \
-people to the newly discovered habitable planet.")
-print("You have left your planet for a long trip \
-through the vast space in search of a better life.\n")
-print("You are suddenly awoken. You woke up before all other travellers, \
-and you need to find out what is going on.")
-print("Are you ready to open and leave your hibernation pod? [y/n]\n")
+def start_game_message():
+    print("Welcome to your spaceship adventure!")
+    print("It is a year 3076. You are an interplanetary traveler, "
+          "at least that is what they said you will be. \n")
+    print("As due to an overpopulation on our home planet Earth"
+          "the collective government devised a plan to send three million "
+          "people to the newly discovered habitable planet.")
+    print("You have left your planet for a long trip "
+          "through the vast space in search of a better life.\n")
+    print("You are suddenly awoken. You woke up before all other travellers, "
+          "and you need to find out what is going on.")
+    print("Are you ready to open and leave your hibernation pod? [y/n]\n")
 
 
 def open_hibernation_pod():
@@ -19,15 +20,17 @@ def open_hibernation_pod():
     Runs a while loop until user decides to leave the hibernation pod.
     Sets up the decision on how to progress through the game.
     """
+    start_game_message()
+
     while True:
         pod_choice = input("> ").lower()
         if pod_choice == "yes" or pod_choice == "y":
-            print("With a little bit of struggle, the pod creaks open")
-            print("You step out but your legs falter, you start falling")
-            print("As you are falling, you decide: \n\
-                (a) grab onto the pod door \n\
-                (b) grab a cord hanging from the ceiling \n\
-                (c) risk injury from the fall")
+            print("\nWith a little bit of struggle, the pod creaks open")
+            print("You step out but your legs falter, you start falling.\n")
+            print("As you are falling, you decide: \n"
+                  "(a) grab onto the pod door \n"
+                  "(b) grab a cord hanging from the ceiling \n"
+                  "(c) risk injury from the fall\n")
             fall_choice = input("What do you do? [a/b/c] \n> ")
             return fall_choice
         elif pod_choice == "no" or pod_choice == "n":
@@ -35,27 +38,79 @@ def open_hibernation_pod():
             print("You fell asleep. \n")
             print("//////////////////////////////////////////\n")
             print("Let's try again.")
-            print("Are you ready to open and leave the hibernation pod? [y/n]")
+            print("Are you ready to open and leave the hibernation pod? [y/n]\n")
         else:
             print("\nInvalid input. Please choose either yes/y or no/n")
 
 
+def restart_game_choice():
+    restart_choice = input("Do you want to restart the game? [y/n] \n> ")
+    if restart_choice == "y" or restart_choice == "yes":
+        print("\nRestarting game!\n")
+        open_hibernation_pod()
+    elif restart_choice == "n" or restart_choice == "no":
+        print("Thank you for playing!")
+        print("Closing the game")
+
+
+fall_choice = None
+
 fall_choice = open_hibernation_pod()
+
+
+def bleeding_wound():
+    """
+    Handles user choice to fall and deals damage until user either finds
+    bandages or ends up bleeding to death.
+    """
+
+
+def choose_path(fall_choice):
+    """
+    Sets up the game progress depending on users choices.
+    """
+    global path_choice
+    while True:
+        path_choice = input("Which path would you like to \
+take? [a/b] \n> ").lower()
+
+        if path_choice == "a" and fall_choice == "a":
+            print("You head straight towards the deck.")
+            print("Eager to continue exploring.")
+        elif path_choice == "a" and fall_choice == "c":
+            print("You head straight towards the deck.\n")
+            bleeding_wound()
+            # future function that will handle bleeding outcome
+        elif path_choice == "a" and fall_choice == "c":
+            print("You head right towards the infirmary.")
+            print("Eager to continue exploring.")
+        elif path_choice == "b" and fall_choice == "c":
+            print("You head right towards the infirmary.")
+            print("Where hopefully you will find "
+                  "the bandages needed to stop the bleeding.\n")
+            bleeding_wound()
+            # future function that will handle bleeding outcome
+        else:
+            print("Invalid input. Please choose [a/b]")
+
 
 if fall_choice == "a":
     print("You managed to grab onto the pod door")
     print("You regained your balance and start exploring the room.")
     print("You see two paths, you can either go straight \
 towards the ship deck, or to the right towards the infirmary.")
+    choose_path("a")
 elif fall_choice == "b":
     print("You accidentally grabbed the electrical cord and got electrocuted.")
     print("You died.")
     print("The End.")
+    restart_game_choice()
 elif fall_choice == "c":
     print("You fell and cut your hand; you are bleeding.")
     print("You should find some bandages to cover the wound.")
     print("You start exploring the room.")
-    print("You see two paths, you can either go straight towards the deck \
-or to the right towards the infirmary.")
+    print("You see two paths, you can either go straight towards the deck[a] \
+or to the right towards the infirmary.[b]")
+    choose_path("c")
 else:
     print("Invalid input, please choose [a/b/c]")
