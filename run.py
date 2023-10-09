5a0cc86641b68988c0ad2963e5d33ec55a34eec3
LukaB25 <>
2023-10-09 17:33:50 +0000

    minor changes to fix the existing code prior to expanding the game.

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 print("SPACESHIP ADVENTURE \n")
 
+
 def start_game_message():
     """
     Prints out the welcome message and some details to set the scene.
@@ -8,7 +9,7 @@ def start_game_message():
     print("Welcome to your spaceship adventure!")
     print("It is a year 3076. You are an interplanetary traveler, "
           "at least that is what they said you will be. \n")
-    print("As due to an overpopulation on our home planet Earth"
+    print("Due to an overpopulation on our home planet Earth"
           "the collective government devised a plan to send three million "
           "people to the newly discovered habitable planet.")
     print("You have left your planet for a long trip "
@@ -27,9 +28,9 @@ def open_hibernation_pod():
     while True:
         pod_choice = input("> ").lower()
         if pod_choice == "yes" or pod_choice == "y":
-            print("\nWith a little bit of struggle, the pod creaks open")
+            print("\nWith a little bit of struggle, the pod creaks open.")
             print("You step out but your legs falter, you start falling.\n")
-            print("As you are falling, you decide: \n"
+            print("As you are falling, you decide to: \n"
                   "(a) grab onto the pod door \n"
                   "(b) grab a cord hanging from the ceiling \n"
                   "(c) risk injury from the fall\n")
@@ -40,21 +41,26 @@ def open_hibernation_pod():
             print("You fell asleep. \n")
             print("//////////////////////////////////////////\n")
             print("Let's try again.")
-            print("Are you ready to open and leave the hibernation pod? [y/n]\n")
+            print("Are you ready to open and leave the hibernation pod? "
+                  "[y/n]\n")
         else:
             print("\nInvalid input. Please choose either yes/y or no/n")
 
 
-def restart_game_choice():
-    restart_choice = input("Do you want to restart the game? [y/n] \n> ")
-    if restart_choice == "y" or restart_choice == "yes":
-        print("\nRestarting game!\n")
-        start_game_message()
-        open_hibernation_pod()
-    elif restart_choice == "n" or restart_choice == "no":
-        print("Thank you for playing!")
-        print("Closing the game")
-        quit()
+# def restart_game_choice():
+#     global fall_choice  # Updates the global fall_choice
+#     global cut_state    # Updates the global cut_state
+#     restart_choice = input("Do you want to restart the game? [y/n] \n> ")
+#     if restart_choice == "y" or restart_choice == "yes":
+#         print("\nRestarting game!\n")
+#         fall_choice = None
+#         cut_state = 0
+#         start_game_message()
+#         open_hibernation_pod()
+#     elif restart_choice == "n" or restart_choice == "no":
+#         print("Thank you for playing!")
+#         print("Closing the game")
+#         quit()
 
 
 fall_choice = None
@@ -66,9 +72,9 @@ def bleeding_wound(cut_state):
     Handles user choice to fall and deals damage until user either finds
     bandages or reaches critical state.
     """
-    
+
     cut_state += 1
-    if cut_state >=5:
+    if cut_state >= 5:
         print("You are slowly bleeding out, "
               "find some bandages quickly.")
     elif cut_state == 6:
@@ -95,8 +101,8 @@ take? [a/b] \n> ").lower()
             print("Eager to continue exploring.")
         elif path_choice == "a" and fall_choice == "c":
             print("You head straight towards the deck.\n")
-            bleeding_wound()
-            # future function that will handle bleeding outcome
+            cut_state = bleeding_wound(cut_state)
+            # Updates the cut_state
         elif path_choice == "a" and fall_choice == "c":
             print("You head right towards the infirmary.")
             print("Eager to continue exploring.")
@@ -104,11 +110,12 @@ take? [a/b] \n> ").lower()
             print("You head right towards the infirmary.")
             print("Where hopefully you will find "
                   "the bandages needed to stop the bleeding.\n")
-            bleeding_wound()
-            # future function that will handle bleeding outcome
+            cut_state = bleeding_wound(cut_state)
+            # Updates the cut_state
         else:
             print("Invalid input. Please choose [a/b]")
             quit()
+
 
 def main():
     start_game_message()
@@ -116,25 +123,28 @@ def main():
 
     if fall_choice == "a":
         print("You managed to grab onto the pod door")
-        print("You regained your balance and start exploring the room.")
+        print("You regained your balance and start exploring the room./n")
         print("You see two paths, you can either go straight "
               "towards the ship deck, or to the right towards the infirmary.")
         choose_path("a")
     elif fall_choice == "b":
-        print("You accidentally grabbed the electrical cord and got electrocuted.")
+        print("You accidentally grabbed the electrical cord "
+              "and got electrocuted.")
         print("You died.")
         print("The End.")
-        restart_game_choice()
+        quit()
     elif fall_choice == "c":
         print("You fell and cut your hand; you are bleeding.")
-        print("You should find some bandages to cover the wound.")
-        print("You start exploring the room.")
-        print("You see two paths, you can either go straight towards the deck[a] "
+        print("You should find some bandages to cover the wound.\n")
+        print("You start exploring the room.\n")
+        print("You see two paths, you can either go "
+              "straight towards the deck[a] "
               "or to the right towards the infirmary.[b]")
         choose_path("c")
     else:
-        print("You fell and back into the hibernation pod and lost consciousness.")
-        restart_game_choice()
+        print("You fell and back into the hibernation pod "
+              "and lost consciousness.")
+
 
 if __name__ == "__main__":
     main()
