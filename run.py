9fc56e47d4ee5b485e587d6c8e5bb1bf898495a6
LukaB25 <>
2023-10-09 21:38:18 +0000

    Creating a med bay function and adding progress to the story.

--- run.py
+++ run.py
@@ -77,6 +77,7 @@ def open_hibernation_pod():
 fall_choice = None
 cut_state = 0
 navigation_threat = 0
+bleeding = False
 
 
 def bleeding_wound(cut_state):
@@ -84,17 +85,19 @@ def bleeding_wound(cut_state):
     Handles user choice to fall and deals damage until user either finds
     bandages or reaches critical state.
     """
-
-    cut_state += 1
-    if cut_state <= 5:
-        print("You are slowly bleeding out, "
-              "find some bandages quickly.")
-        print(f"You are hurt, reach the Med Bay: {cut_state}/6")
-    elif cut_state == 6:
-        print(f"You reached critical state {cut_state}/6. "
-              "You didn't reach Med Bay.")
-        print("You died.")
-        quit()
+    global bleeding
+    if bleeding:
+        cut_state += 1
+        if cut_state <= 5:
+            print("You are slowly bleeding out, "
+                  "find some bandages quickly.")
+            print(f"You are hurt, reach the Med Bay: {cut_state}/6")
+        elif cut_state == 6:
+            print(f"You reached critical state {cut_state}/6. "
+                  "You didn't reach Med Bay.")
+            print("You died.")
+            bleeding = False
+            quit()
     return cut_state
 
 
@@ -119,6 +122,7 @@ def navigation_failure(navigation_threat):
               f"is imminent. Level: {navigation_threat}/10")
         print("Manual reboot necessary.\n")
     else:
+        clear()
         print("\nNavigation failure! Critical state "
               f"reached. Level: {navigation_threat}/10\n"
               "The ship has entered an asteroid field.\n")
@@ -149,7 +153,7 @@ def control_room_choose_path():
     while True:
         control_path_choice = input("Which path would you like to "
                                     "take? [a/b] \n> ").lower()
-        if control_path_choice == "a":
+        if control_path_choice == "a" or control_path_choice == "cargo hold":
             print("You head left towards the Cargo Hold.")
             if cut_state > 0:
                 cut_state = bleeding_wound(cut_state)
@@ -157,7 +161,10 @@ def control_room_choose_path():
             if navigation_threat > 0:
                 navigation_threat = navigation_failure(navigation_threat)
                 # Updates the navigation_threat
-        elif control_path_choice == "b":
+        elif (control_path_choice == "b" or
+              control_path_choice == "engineering bay" or
+              control_path_choice == "engineering" or
+              control_path_choice == "engineer"):
             print("You head right towards the Engineering Bay.")
             print("Wondering what might you find there.")
             if cut_state > 0:
@@ -208,6 +215,95 @@ def control_room():
     control_room_choose_path()
 
 
+def healing_procedure():
+    """
+    Healing procedure that runs after the user arrives to the Med Bay
+    and decides to try using the Regenesis Chamber 3000. It updates
+    the bleeding variable to False to disable the bleeding_wound function.
+    """
+    global bleeding
+
+    print("Scanning the wound.")
+    print("Decontamination")
+    print("Tissue Regeneration")
+    print("Cellular Rejuvenation")
+    print("Completion")
+    bleeding = False
+
+
+def forceful_healing_procedure():
+    """
+    Forceful healing procedure that runs after the user arrives to the Med Bay
+    and decides to try using the Regenesis Chamber 3000 but decides they don't
+    want to heal an existing wound. The system tries to force a
+    healing_procedure onto the user, which ends up ending users life and the
+    Regenesis Chamber 3000 starts shutting down.
+    """
+    print("\nSpecimen denies healing procedure.. Error..")
+    print("Forcefull healing engaged.")
+    print(("Restraints engaged!\n").upper())
+    healing_procedure()
+    print("\nInjecting a short term memory loss serum.")
+    print("Error.. error..")
+    print("Specimen has received the wrong serum.\n")
+    print("Regene.. \nCham.. \n30.. shut.. \ndow..")
+    print("\n/////////////////\n")
+    print("You have received a deadly dose of serum.")
+    print("You died.")
+    print("The End")
+    quit()
+
+
+def med_bay_choose_path():
+    """
+    Handles the player's actions and choices when in the Med Bay.
+    The user can choose to continue exploring the spaceship and continue
+    moving through the rest of the rooms left to explore.
+    """
+
+
+def med_bay():
+    """
+    Handles the player's actions and choices when they reach the Med Bay.
+    After a user reaches Med Bay they can choose to either check their health,
+    heal their wound and stop the bleeding if they were damaged at the start
+    or they can continue with their exploration of the spaceship.
+    """
+    clear()
+
+    print("You enter the Med Bay. Straight away you notice "
+          "all of the medical supplies. You are mesmerised "
+          "by the size of it, you couldn\'t even imagine "
+          "something like that would fit onto the ship.\n")
+
+    print("Amongst the sea of medical supplies, you notice a "
+          "machine in the middle of the room. You approach it.")
+    print("\nRegenesis Chamber 3000. Sounds so majestic.\n")
+    print("You remember all of the advertising the collective "
+          "government made, trying to sell them for at "
+          "home use back in the day.\n")
+
+    healing_chamber = input("Would you like to test your health? [y/n] \n> ")
+
+    if healing_chamber == "y" or healing_chamber == "yes":
+        print("\nScanning...\n")
+        print("Scan complete!")
+        if bleeding:
+            print("Anomaly detected.")
+            print("Wound detected on the left forearm.")
+            heal_wound = input("Would you like to heal the wound? [y/n] \n> ")
+
+            if heal_wound == "y" or heal_wound == "yes":
+                healing_procedure()
+            elif heal_wound == "n" or heal_wound == "no":
+                forceful_healing_procedure()
+            print("Thank you for using Regenesis Chamber 3000")
+        else:
+            print("No anomalies detected.")
+            print("\nYou are a healthy specimen.\n")
+        print("Thank you for using Regenesis Chamber 3000")
+
+
 def choose_path(fall_choice):
     """
     Sets up the game progress depending on users choices.
@@ -226,21 +322,24 @@ def choose_path(fall_choice):
             cut_state = bleeding_wound(cut_state)
             control_room()
             # Updates the cut_state
-        elif path_choice == "a" and fall_choice == "c":
+        elif path_choice == "b" and fall_choice == "a":
             print("You head right towards the Med Bay.")
             print("Eager to continue exploring.")
+            med_bay()
         elif path_choice == "b" and fall_choice == "c":
             print("You head right towards the Med Bay.")
             print("Where hopefully you will find "
                   "the bandages needed to stop the bleeding.\n")
             cut_state = bleeding_wound(cut_state)
             # Updates the cut_state
+            med_bay()
         else:
             print("Invalid input. Please choose [a/b]")
             quit()
 
 
 def main():
+    global bleeding
     start_game_message()
     fall_choice = open_hibernation_pod()
 
@@ -265,6 +364,7 @@ def main():
         print("You see two paths, you can either go "
               "straight towards the Control Room[a] "
               "or to the right towards the Med Bay.[b]")
+        bleeding = True
         choose_path(fall_choice)
     else:
         print("You fell and back into the hibernation pod "
